6a6054aa03c5982ab0a3a9b86230beec10ec3016
zhm-1997 <zhanghhhmin@163.com>
2019-12-28 12:09:18 +0800

    green

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,6 +1,9 @@
 public class FizzBuzz {
     public static void main(String[] args){}
     public static String of(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
 
         return null;
     }
