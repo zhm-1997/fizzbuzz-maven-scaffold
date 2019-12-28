f979194fde73375a67ac78edc9489752602bcff8
zhm-1997 <zhanghhhmin@163.com>
2019-12-28 16:54:48 +0800

    final refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,14 +1,23 @@
 public class FizzBuzz {
-    public static void main(String[] args){}
     public static String of(int number) {
+        String s = "";
+        if (number <= 0) {
+            throw new IllegalArgumentException("Invalid Input");
+        }
         if (number % 3 == 0) {
-            return "Fizz";
+            s += "Fizz";
         }
         if (number % 5 == 0) {
-            return "Buzz";
+            s += "Buzz";
+        }
+        if (number % 7 == 0) {
+            s += "Whizz";
         }
         if (number % 15 == 0) {
-            return "FizzBuzz";
+            s += "FizzBuzz";
+        }
+        if (s.length() == 0) {
+            s += number;
         }
         return null;
     }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,12 +1,22 @@
 import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
+    public void should_throw() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("Invalid input");
+    }
+
     public void should_return_Fizz() {
-        int number=3;
-        String result= FizzBuzz.of(number);
+        int number = 3;
+        String result = FizzBuzz.of(number);
         assertThat(result).isEqualTo("Fizz");
     }
 
