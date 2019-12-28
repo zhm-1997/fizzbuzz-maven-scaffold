3deb00c43fbed2c35c87365737e07f0749e05b00
zhm-1997 <zhanghhhmin@163.com>
2019-12-28 12:09:55 +0800

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,7 +4,12 @@ public class FizzBuzz {
         if (number % 3 == 0) {
             return "Fizz";
         }
-
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        }
         return null;
     }
 }
